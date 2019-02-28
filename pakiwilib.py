bdffcfc83dfd5e0763c3e26bd03b47d821460d11
Alexandre Manoury <alexandre.manoury@gmail.com>
2019-02-28 18:38:30 +0100

    Updated lib

--- pakiwilib.py
+++ pakiwilib.py
@@ -1,6 +1,8 @@
 import os
 import time
 import yaml
+import pickle
+import numpy as np
 
 
 class UnimplementedMethod(Exception):
@@ -11,7 +13,7 @@ class Set(object):
     INPUT_FOLDER = 'inputs'
     INPUT_EXTENSION = '.in'
 
-    def __init__(self, setname, input_folder='inputs', input_extension='in'):
+    def __init__(self, setname):
         self.setname = setname
         self.filename = os.path.join(self.INPUT_FOLDER, '{}{}'.format(self.setname, self.INPUT_EXTENSION))
         self._load()
@@ -19,7 +21,7 @@ class Set(object):
     def _load(self):
         if not os.path.exists(self.filename):
             raise Exception('The input dataset file {} does not exist'.format(self.filename))
-        with open(path) as f:
+        with open(self.filename) as f:
             content = f.readlines()
         self._processData(content)
 
@@ -29,14 +31,18 @@ class Set(object):
     def score(self, result):
         raise UnimplementedMethod('Please implement score() in a subclass of Set')
 
+    def raw_score(self, result):
+        raise UnimplementedMethod('Please implement score() in a subclass of Set')
+
 
 class Result(object):
     OUTPUT_FOLDER = 'outputs'
     OUTPUT_EXTENSION = '.out'
     OUTPUT_METADATA = 'meta.yml'
+    OUTPUT_DATAFILE = 'data.pickle'
 
-    def __init__(self, set):
-        self.set = set
+    def __init__(self, set_):
+        self.set = set_
         self.date = time.time()
         self.foldername = None
 
@@ -44,16 +50,19 @@ class Result(object):
         return self.set.score(self)
 
     @classmethod
-    def list():
+    def list(cls):
         if not os.path.exists(cls.OUTPUT_FOLDER):
             return []
-        reults = []
+        results = []
         for f in os.listdir(cls.OUTPUT_FOLDER):
-            if os.path.isdir(f):
-                with open(os.path.join(f, cls.OUTPUT_METADATA), 'r') as stream:
+            filename = os.path.join(cls.OUTPUT_FOLDER, f)
+            if os.path.isdir(filename):
+                with open(os.path.join(filename, cls.OUTPUT_METADATA), 'r') as stream:
                     meta = yaml.load(stream)
+                print(meta)
                 score = meta.get('score', -1)
-                results.append((f, score))
+                date = meta.get('date', 0)
+                results.append((f, score, date))
         return results
 
     def save(self):
@@ -71,7 +80,8 @@ class Result(object):
 
         # create the output folder for this results
         self.foldername = foldername(index)
-        os.mkdir(self.fodlername)
+        self.datafilename = os.path.join(self.foldername, self.OUTPUT_DATAFILE)
+        os.mkdir(self.foldername)
 
         # save metadata
         meta = {
@@ -80,3 +90,53 @@ class Result(object):
         }
         with open(os.path.join(self.foldername, self.OUTPUT_METADATA), 'w') as stream:
             yaml.dump(meta, stream, default_flow_style=False)
+
+        self._save_data()
+
+    @classmethod
+    def load(cls, resultname, set_=None):
+        foldername = os.path.join(cls.OUTPUT_FOLDER, resultname)
+        with open(os.path.join(foldername, cls.OUTPUT_METADATA), 'r') as stream:
+            meta = yaml.load(stream)
+        score = meta.get('score', -1)
+        date = meta.get('date', 0)
+
+        datafilename = os.path.join(foldername, cls.OUTPUT_DATAFILE)
+
+        result = cls._load_data(datafilename, set_)
+        result.date = date
+        result.foldername = foldername
+        return result
+
+    def _save_data(self):
+        raise UnimplementedMethod('Please implement _save_data() in a subclass of Set')
+
+    @classmethod
+    def _load_data(cls, datafile, set_):
+        raise UnimplementedMethod('Please implement _load_data() in a subclass of Set')
+
+
+class SampleSet(Set):
+    def _processData(self, data):
+        self.data = data
+
+    def score(self, result):
+        return self.raw_score(result.data)
+
+    def raw_score(self, data):
+        return int(np.sum(data))
+
+
+class SampleResult(Result):
+    def __init__(self, set_, data):
+        super(SampleResult, self).__init__(set_)
+        self.data = data
+
+    def _save_data(self):
+        pickle.dump(self.data, open(self.datafilename, 'wb'))
+
+    @classmethod
+    def _load_data(cls, datafile, set_):
+        data = pickle.load(open(datafile, 'rb'))
+        result = cls(set_, data)
+        return result
